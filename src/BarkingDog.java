3d9f7abaade791c7713c2cc7052d59d0dcd6a174
MichalLabencki <74935701+MichalLabencki@users.noreply.github.com>
2021-01-15 15:03:32 +0100

    Initial commit

---
+++ src/BarkingDog.java
@@ -0,0 +1,14 @@
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (barking) {
+            if (hourOfDay < 8 && hourOfDay >= 0 || hourOfDay > 22 && hourOfDay < 24) {
+                return true;
+            } else {
+                return false;
+            }
+        } else
+            return false;
+    }
+}
+
